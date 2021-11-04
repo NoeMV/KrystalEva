8910d75f95e34030a68e62071a0bbe11d422b4f2
olegyasikov <91714648+olegyasikov@users.noreply.github.com>
2021-11-03 18:27:43 -0600

    Cambio de tamaño
    Cambio en tamaño del main

--- Krystal3/Krystal3/Form1.Designer.cs
+++ Krystal3/Krystal3/Form1.Designer.cs
@@ -46,7 +46,7 @@ namespace Krystal3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1309, 757);
             this.Controls.Add(this.label1);
             this.Name = "Main";
             this.Text = "Main";
