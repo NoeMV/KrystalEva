4382e246ae2a0eaf07afad8ba95b68532c4223d9
olegyasikov <91714648+olegyasikov@users.noreply.github.com>
2021-11-16 17:20:04 -0600

    Colaboradores consulta + diseño kk

--- Krystal3/Krystal3/Colaboradores.Designer.cs
+++ Krystal3/Krystal3/Colaboradores.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.colaborador_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.curp = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -45,6 +49,22 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colaborador_id,
@@ -58,9 +78,40 @@
             this.claveNivelEstudios,
             this.claveDocProbatorio,
             this.claveInstitucion});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Silver;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1143, 352);
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1293, 352);
             this.dataGridView1.TabIndex = 0;
             // 
             // colaborador_id
@@ -68,64 +119,98 @@
             this.colaborador_id.HeaderText = "#";
             this.colaborador_id.Name = "colaborador_id";
             this.colaborador_id.ReadOnly = true;
+            this.colaborador_id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.colaborador_id.Width = 43;
             // 
             // curp
             // 
             this.curp.HeaderText = "CURP";
             this.curp.Name = "curp";
+            this.curp.ReadOnly = true;
+            this.curp.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.curp.Width = 82;
             // 
             // nombre
             // 
             this.nombre.HeaderText = "NOMBRE";
             this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            this.nombre.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.nombre.Width = 109;
             // 
             // primerApellido
             // 
             this.primerApellido.HeaderText = "PRIMER APELLIDO";
             this.primerApellido.Name = "primerApellido";
+            this.primerApellido.ReadOnly = true;
+            this.primerApellido.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.primerApellido.Width = 176;
             // 
             // segundoApellido
             // 
             this.segundoApellido.HeaderText = "SEGUNDO APELLIDO";
             this.segundoApellido.Name = "segundoApellido";
+            this.segundoApellido.ReadOnly = true;
+            this.segundoApellido.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.segundoApellido.Width = 194;
             // 
             // claveEstado
             // 
             this.claveEstado.HeaderText = "CLAVE ESTADO";
             this.claveEstado.Name = "claveEstado";
+            this.claveEstado.ReadOnly = true;
+            this.claveEstado.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.claveEstado.Width = 153;
             // 
             // municipio_id
             // 
             this.municipio_id.HeaderText = "CLAVE MUNICIPIO";
             this.municipio_id.Name = "municipio_id";
+            this.municipio_id.ReadOnly = true;
+            this.municipio_id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.municipio_id.Width = 172;
             // 
             // ocupacion_id
             // 
             this.ocupacion_id.HeaderText = "CLAVE OCUPACIÓN";
             this.ocupacion_id.Name = "ocupacion_id";
+            this.ocupacion_id.ReadOnly = true;
+            this.ocupacion_id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ocupacion_id.Width = 182;
             // 
             // claveNivelEstudios
             // 
             this.claveNivelEstudios.HeaderText = "CLAVE NIV ESTUDIOS";
             this.claveNivelEstudios.Name = "claveNivelEstudios";
+            this.claveNivelEstudios.ReadOnly = true;
+            this.claveNivelEstudios.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.claveNivelEstudios.Width = 202;
             // 
             // claveDocProbatorio
             // 
             this.claveDocProbatorio.HeaderText = "CLAVE DOC PROBATORIO";
             this.claveDocProbatorio.Name = "claveDocProbatorio";
+            this.claveDocProbatorio.ReadOnly = true;
+            this.claveDocProbatorio.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.claveDocProbatorio.Width = 231;
             // 
             // claveInstitucion
             // 
             this.claveInstitucion.HeaderText = "CLAVE INSTITUCION";
             this.claveInstitucion.Name = "claveInstitucion";
+            this.claveInstitucion.ReadOnly = true;
+            this.claveInstitucion.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.claveInstitucion.Width = 189;
             // 
             // Colaboradores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1181, 382);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(243)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(1330, 391);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Colaboradores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Colaboradores";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
