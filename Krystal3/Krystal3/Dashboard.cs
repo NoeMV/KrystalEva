32eecac84992aba34241dd69c343d824336e8920
NoeMV <kilonoe@gmail.com>
2021-11-30 21:37:24 -0600

    reportes sin formato

--- Krystal3/Krystal3/Dashboard.cs
+++ Krystal3/Krystal3/Dashboard.cs
@@ -51,7 +51,7 @@ namespace Krystal3
 
         private void BtnReportes_Click(object sender, EventArgs e)
         {
-            //AbrirFormulario(new Reportes());
+            
         }
 
         private void BtnEstadisticas_Click(object sender, EventArgs e)
@@ -86,6 +86,11 @@ namespace Krystal3
         private void PanelBarra_MouseUp(object sender, MouseEventArgs e)
         {
             mouseDown = false;
+        }
+
+        private void BtnReportes_Click_1(object sender, EventArgs e)
+        {
+            AbrirFormulario(new Reportes());
         }
     }
 }
