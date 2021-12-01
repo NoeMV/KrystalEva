4f5abeba13c98f521d58e6464a5a6e81396bdf76
4ntoniov <4ntonio.villegas@gmail.com>
2021-11-30 22:38:17 -0600

    Estadisticas

---
+++ Krystal3/Krystal3/Estadisticas.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Krystal3
+{
+    partial class Estadisticas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chartCursos = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCursos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartCursos
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartCursos.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartCursos.Legends.Add(legend1);
+            this.chartCursos.Location = new System.Drawing.Point(323, 22);
+            this.chartCursos.Name = "chartCursos";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartCursos.Series.Add(series1);
+            this.chartCursos.Size = new System.Drawing.Size(380, 351);
+            this.chartCursos.TabIndex = 0;
+            this.chartCursos.Text = "Cursos";
+            title1.Name = "Diciembre";
+            title2.Name = "Enero";
+            this.chartCursos.Titles.Add(title1);
+            this.chartCursos.Titles.Add(title2);
+            // 
+            // Estadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.chartCursos);
+            this.Name = "Estadisticas";
+            this.Text = "Estadisticas";
+            this.Load += new System.EventHandler(this.Estadisticas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartCursos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCursos;
+    }
+}
---
+++ Krystal3/Krystal3/Estadisticas.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krystal3
+{
+    public partial class Estadisticas : Form
+    {
+        public Estadisticas()
+        {
+            InitializeComponent();
+        }
+
+        private void Estadisticas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int cantidadCursos(String sql)
+        {
+
+            String miConexion = ConfigurationManager.ConnectionStrings["NombreConexion"].ConnectionString;
+            SqlConnection Conexion = new SqlConnection(miConexion);
+            int cantCursos = 0;
+            try
+            {
+                SqlCommand command = new SqlCommand(sql, Conexion);
+                Conexion.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    cantCursos = Convert.ToInt32(reader.GetString(0));
+
+                }
+                Conexion.Close();
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                MessageBox.Show("Ha ocurrido un error al obtener la cantidad de cursos");
+            }
+
+            return cantCursos;
+
+        }
+
+        public void llenarCursos()
+        {
+            int enero = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-01-01' AND fechaFin<= '2021-01-31'");
+            int febrero = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-02-01' AND fechaFin<= '2021-02-28'");
+            int marzo = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-03-01' AND fechaFin<= '2021-03-31'");
+            int abril = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-04-01' AND fechaFin<= '2021-04-30'");
+            int mayo = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-05-01' AND fechaFin<= '2021-05-31'");
+            int junio = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-06-01' AND fechaFin<= '2021-06-30'");
+            int julio = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-07-01' AND fechaFin<= '2021-07-31'");
+            int agosto = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-08-01' AND fechaFin<= '2021-08-31'");
+            int septiembre = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-09-01' AND fechaFin<= '2021-09-30'");
+            int octubre = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-01-10' AND fechaFin<= '2021-10-31'");
+            int noviembre = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-11-01' AND fechaFin<= '2021-11-30'");
+            int diciembre = cantidadCursos("SELECT count(*) FROM Cursos where status=1 AND fechaInicio>= '2021-12-01' AND fechaFin<= '2021-12-31'");
+
+
+
+
+
+
+        }
+
+    }
+    
+}
